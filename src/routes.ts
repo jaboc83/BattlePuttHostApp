f44ea133f7e30c4b538312f415090ef5117969a1
Jake Moening <jaboc83@gmail.com>
2022-12-15 11:08:04 -0600

    Update protocol

--- src/routes.ts
+++ src/routes.ts
@@ -4,5 +4,5 @@ export const rejoin = '/rejoin';
 export const start = '/start';
 export const clientUrl =
   process.env.NODE_ENV === 'development'
-    ? 'https://localhost:3002/BattlePuttClientApp'
+    ? 'http://localhost:3002/BattlePuttClientApp'
     : 'https://github.com/jaboc83/BattlePuttClientApp';
